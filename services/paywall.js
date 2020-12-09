e87a816eeb1bdc1711f9c05fb8ff7dc0eb3c69a3
Farhan Ali <farhan.ali@dmdmax.com>
2020-12-09 15:32:22 +0500

    tps count ip changed to paywall production from staging

--- services/paywall.js
+++ services/paywall.js
@@ -21,7 +21,7 @@ tokenRenewal = async () => {
 resetDailyAmountSpentByUser = async () => {
     try {
         axios.get(config.paywall_service_url + '/cron/dailyAmoutReset');
-        axios.get('http://10.0.1.21:5001/cron/dailyAmountReset');
+        axios.get('http://10.0.1.76:5001/cron/dailyAmountReset');
     } catch(er) {
         console.error(er);
     }
@@ -29,7 +29,7 @@ resetDailyAmountSpentByUser = async () => {
 
 resetTpsCount = async () => {
     try {
-        axios.get('http://10.0.1.21:5001/cron/tpsCountReset');
+        axios.get('http://10.0.1.76:5001/cron/tpsCountReset');
         axios.get(config.paywall_service_url + '/cron/tpsCountReset/');
     } catch(er) {
         console.error(er);
