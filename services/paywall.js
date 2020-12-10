f939ed28129291df2ae87723564a100af4dac2c5
Farhan Ali <farhan.ali@dmdmax.com>
2020-12-10 13:37:54 +0000

    Update paywall.js

--- services/paywall.js
+++ services/paywall.js
@@ -20,7 +20,7 @@ tokenRenewal = async () => {
 
 resetDailyAmountSpentByUser = async () => {
     try {
-        axios.get('http://10.0.1.76:5001/cron/dailyAmountReset');
+        //axios.get('http://10.0.1.76:5001/cron/dailyAmountReset');
         axios.get(config.paywall_service_url + '/cron/dailyAmoutReset');
     } catch(er) {
         console.error(er);
@@ -29,7 +29,7 @@ resetDailyAmountSpentByUser = async () => {
 
 resetTpsCount = async () => {
     try {
-        axios.get('http://10.0.1.76:5001/cron/tpsCountReset');
+        //axios.get('http://10.0.1.76:5001/cron/tpsCountReset');
         axios.get(config.paywall_service_url + '/cron/tpsCountReset/');
     } catch(er) {
         console.error(er);
